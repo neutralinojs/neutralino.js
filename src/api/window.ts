b047326bb6bd9fc9c59fea0ea43aa7bb64a750d6
Shalitha Suranga <shalithasuranga@gmail.com>
2021-07-15 18:26:52 +0530

    Adding new API methods to window

--- src/api/window.ts
+++ src/api/window.ts
@@ -27,7 +27,7 @@ export function unmaximize(): Promise<any> {
     });
 };
 
-export function isMaximized(): Promise<any> {
+export function isMaximized(): Promise<boolean> {
     return request({
         url : 'window.isMaximized',
         type : RequestType.GET,
@@ -40,5 +40,72 @@ export function minimize(): Promise<any> {
         url : 'window.minimize',
         type : RequestType.GET,
         isNativeMethod: true
+    });
+};
+
+export function setFullScreen(): Promise<any> {
+    return request({
+        url : 'window.setFullScreen',
+        type : RequestType.GET,
+        isNativeMethod: true
+    });
+};
+
+export function exitFullScreen(): Promise<any> {
+    return request({
+        url : 'window.exitFullScreen',
+        type : RequestType.GET,
+        isNativeMethod: true
+    });
+};
+
+export function isFullScreen(): Promise<boolean> {
+    return request({
+        url : 'window.isFullScreen',
+        type : RequestType.GET,
+        isNativeMethod: true
+    });
+};
+
+export function show(): Promise<any> {
+    return request({
+        url : 'window.show',
+        type : RequestType.GET,
+        isNativeMethod: true
+    });
+};
+
+export function hide(): Promise<any> {
+    return request({
+        url : 'window.hide',
+        type : RequestType.GET,
+        isNativeMethod: true
+    });
+};
+
+export function isVisible(): Promise<boolean> {
+    return request({
+        url : 'window.isVisible',
+        type : RequestType.GET,
+        isNativeMethod: true
+    });
+};
+
+export function focus(): Promise<any> {
+    return request({
+        url : 'window.focus',
+        type : RequestType.GET,
+        isNativeMethod: true
+    });
+};
+
+export function setIcon(icon: string): Promise<any> {
+    return request({
+        url : 'window.setIcon',
+        type : RequestType.POST,
+        isNativeMethod: true,
+        data: {
+            icon
+        }
     });
 };
