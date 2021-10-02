884c81ca8ae0465a5e591bf5747a0e678b80f885
jmv <kitjmv@gmail.com>
2021-10-02 20:31:02 +0200

    Added development version generation

--- build.mjs
+++ build.mjs
@@ -1,16 +1,9 @@
 
-/*/
-    This script generates the files `neutralino.js` and `neutralino.d.ts`.
-    For a development version, use: `node ./build.mjs --dev`
-    this will produce an unminified `neutralino.js` file and the neutralino.d.ts.map file.
-    ISSUE: I don't understand why the source map (`neutralino.js.map`) is not generated.
-/*/
-
 // @ts-check
 
 import { readFileSync, writeFile, mkdirSync, existsSync } from 'fs'
 import { join as joinPath } from 'path'
-import { rollup, watch } from 'rollup'
+import { rollup } from 'rollup'
 import Ts   from 'rollup-plugin-ts'
 import Json from '@rollup/plugin-json'
 import { terser as Minify } from 'rollup-plugin-terser'
@@ -18,21 +11,19 @@ import { terser as Minify } from 'rollup-plugin-terser'
 // JSON modules is experimental https://nodejs.org/api/esm.html#esm_experimental_json_modules
 const { version } = JSON.parse (readFileSync ('./package.json', { encoding: 'utf8' }))
 const outdir  = 'dist'
-const devmode = process.argv.includes ('--dev')
 
 if (existsSync (outdir) === false)
     mkdirSync (outdir, { recursive: true })
 
 console.log ('import src/index.ts')
-
 rollup ({
     input: 'src/index.ts',
     plugins: [
         Json (),
         Ts ({
-            tsconfig: devmode ? config => ({ ...config, declarationMap: true }) : 'tsconfig.json'
+            tsconfig: './tsconfig.json'
         }),
-        devmode ? null : Minify ({ format: { comments: false } })
+        Minify ({ format: { comments: false } })
     ]
 })
 .then (build =>
@@ -52,7 +43,7 @@ rollup ({
 
     /** @type {string} */
     var filepath
-    
+
     // Currently there are only two generated files, the .js and the .d.ts,
     // but maybe in the future if there are dynamic imports, this loop will work.
     for (var entry of output)
