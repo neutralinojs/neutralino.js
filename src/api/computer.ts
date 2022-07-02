91fe8f67b219db2a8e6b7ac07188407fd49c87da
Shalitha Suranga <shalithasuranga@gmail.com>
2022-07-02 16:08:20 +0530

    Add new functions to the computer namespace (#54)
    * Add computer.getArch function
    
    * Add computer.getKernelInfo function
    
    * Add getOSInfo and getDisplays functions to the computer namespace

--- src/api/computer.ts
+++ src/api/computer.ts
@@ -5,6 +5,46 @@ export interface MemoryInfo {
     available: number;
 }
 
+export interface KernelInfo {
+    variant: string;
+    version: string;
+}
+
+export interface OSInfo {
+    name: string;
+    description: string;
+    version: string;
+}
+
+export interface Display {
+    id: number;
+    resolution: Resolution;
+    dpi: number;
+    bpp: number;
+    refreshRate: number;
+}
+
+interface Resolution {
+    width: number;
+    height: number;
+}
+
 export function getMemoryInfo(): Promise<MemoryInfo> {
     return sendMessage('computer.getMemoryInfo');
+};
+
+export function getArch(): Promise<string> {
+    return sendMessage('computer.getArch');
+};
+
+export function getKernelInfo(): Promise<KernelInfo> {
+    return sendMessage('computer.getKernelInfo');
+};
+
+export function getOSInfo(): Promise<OSInfo> {
+    return sendMessage('computer.getOSInfo');
+};
+
+export function getDisplays(): Promise<Display[]> {
+    return sendMessage('computer.getDisplays');
 };
