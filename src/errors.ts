d1cdcd8572ec1a585cac3c33ecb8bfaa2c6abff2
sadaf <sadaf20043006@gmail.com>
2024-02-26 00:02:18 +0530

    feat: sending code along with error message

--- src/errors.ts
+++ src/errors.ts
@@ -19,7 +19,7 @@ export function logError(e) {
     const error = JSON.stringify(e);
     const errorObj = JSON.parse(error);
     if (errorObj && typeof errorObj === 'object' && 'code' in errorObj && 'message' in errorObj) {
-       throw new NeutralinoError({ code: errorObj.code, message: errorObj.message});
+       throw new NeutralinoError({ code: errorObj.code, message: `${errorObj.message} - code : ${errorObj.code}`});
     } else {
         console.error('Invalid error object:', errorObj);
     }
