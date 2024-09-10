334879399ebb9aacf6750097ffc19c7a25a91308
Shalitha Suranga <shalithasuranga@gmail.com>
2024-09-10 14:24:28 +0530

    Export the resources.extractFile function

--- src/api/resources.ts
+++ src/api/resources.ts
@@ -1,5 +1,9 @@
 import { sendMessage } from '../ws/websocket';
 
-export function getFiles(): Promise<void> {
+export function getFiles(): Promise<string[]> {
     return sendMessage('resources.getFiles');
+};
+
+export function extractFile(path: string, destination: string): Promise<void> {
+    return sendMessage('resources.extractFile', { path, destination });
 };
