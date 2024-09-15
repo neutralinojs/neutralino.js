d5968f19dbe9ee28c6d5884c733dbf1ee35b0e19
Shalitha Suranga <shalithasuranga@gmail.com>
2024-09-15 13:49:28 +0530

    Export readFile() and readBinaryFile() functions from the res module

--- src/api/resources.ts
+++ src/api/resources.ts
@@ -1,4 +1,5 @@
 import { sendMessage } from '../ws/websocket';
+import { base64ToBytesArray } from '../helpers';
 
 export function getFiles(): Promise<string[]> {
     return sendMessage('resources.getFiles');
@@ -6,4 +7,20 @@ export function getFiles(): Promise<string[]> {
 
 export function extractFile(path: string, destination: string): Promise<void> {
     return sendMessage('resources.extractFile', { path, destination });
+};
+
+export function readFile(path: string): Promise<string> {
+    return sendMessage('resources.readFile', { path });
+};
+
+export function readBinaryFile(path: string): Promise<ArrayBuffer> {
+    return new Promise((resolve: any, reject: any) => {
+        sendMessage('resources.readBinaryFile', { path })
+        .then((base64Data: string) => {
+            resolve(base64ToBytesArray(base64Data));
+        })
+        .catch((error: any) => {
+            reject(error);
+        });
+    });
 };
