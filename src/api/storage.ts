7e9899571383e328de5e466b6cea31c3c70b5868
Arbaj Ansari <127431775+ArbajAnsari19@users.noreply.github.com>
2024-11-17 04:26:54 +0530

    Fixed Issue : setData() and getData() are not case sensitive for darwin and windows but they are case sensitive for linux

--- src/api/storage.ts
+++ src/api/storage.ts
@@ -1,12 +1,12 @@
 import { sendMessage } from '../ws/websocket';
 
 export function setData(key: string, data: string): Promise<void> {
-    return sendMessage('storage.setData', { key, data });
-};
+    return sendMessage('storage.setData', { key: key.toLowerCase(), data });
+}
 
 export function getData(key: string): Promise<string> {
-    return sendMessage('storage.getData', { key });
-};
+    return sendMessage('storage.getData', { key: key.toLowerCase() });
+}
 
 export function getKeys(): Promise<string[]> {
     return sendMessage('storage.getKeys');
