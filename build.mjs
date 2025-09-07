3213ac7350fb1f89b7a1b03cdd5ce61efaac00f2
dasJulian <julian.szigethy@gmail.com>
2025-09-07 14:19:32 +0200

    Rollup typescript d.ts files

--- build.mjs
+++ build.mjs
@@ -1,145 +1,154 @@
-
 /*/
     This script generates the files `neutralino.js`, `neutralino.mjs`, and `neutralino.d.ts`.
     For a development version, use: `node ./build.mjs --dev`
     this will produce an unminified `neutralino.js` file and the neutralino.js.map file.
     neutralino.js.map can be moved without the source code.
-
-    ISSUE:
-        rollup-plugin-ts produce an empty `neutralino.d.ts.map`
 /*/
 
 // @ts-check
 
-import { readFileSync, writeFile, writeFileSync, mkdirSync, existsSync, readdirSync } from 'fs'
-import { exec } from 'child_process'
-import { join as joinPath } from 'path'
-import { rollup } from 'rollup'
-import Ts from '@rollup/plugin-typescript'
-import Json from '@rollup/plugin-json'
-import Minify from '@rollup/plugin-terser'
+import {
+    readFileSync,
+    writeFile,
+    writeFileSync,
+    mkdirSync,
+    existsSync,
+    readdirSync,
+    rmSync,
+} from 'fs';
+import { exec } from 'child_process';
+import { join as joinPath } from 'path';
+import { rollup } from 'rollup';
+import typescript from '@rollup/plugin-typescript';
+import Json from '@rollup/plugin-json';
+import Minify from '@rollup/plugin-terser';
 import cleanup from 'rollup-plugin-cleanup';
+import { generateDtsBundle } from 'dts-bundle-generator';
 
 // JSON modules is experimental https://nodejs.org/api/esm.html#esm_experimental_json_modules
-const { version } = JSON.parse (readFileSync ('./package.json', { encoding: 'utf8' }))
-const outdir  = 'dist'
-const devmode = process.argv.includes ('--dev')
+const { version } = JSON.parse(
+    readFileSync('./package.json', { encoding: 'utf8' }),
+);
+const outdir = 'dist';
+const devmode = process.argv.includes('--dev');
 
-let commitHash = null
+let commitHash = null;
 
-if (existsSync (outdir) === false)
-    mkdirSync (outdir, { recursive: true })
-
-
-console.log ('Preprocessing files...')
-addCommitHash ()
-
-console.log ('import src/index.ts')
-
-rollup ({
-    input: 'src/index.ts',
-    plugins: [
-        Json (),
-        Ts ({
-            compilerOptions: config => ({
-                ...config,
-                declarationMap: devmode,
-                include: ['src/**/*.ts', 'types/**/*.d.ts']
-            })
-        }),
-        devmode ? cleanup({comments: 'none'}) : Minify ({ format: { comments: false } })
-    ]
-})
-.then (build =>
-{
-    console.log ('generate dist/neutralino.mjs')
-
-    build.write ({
-        file      : joinPath (outdir, 'neutralino.mjs'),
-        format    : 'esm',
-        name      : 'Neutralino',
-        sourcemap : devmode
-    })
-
-    console.log ('generate lib')
-
-    build.write ({
-        dir       : outdir,
-        format    : 'cjs',
-        sourcemap : devmode
-    })
-
-    console.log ('generate dist/neutralino.js')
-
-    return build.generate ({
-        file      : joinPath (outdir, 'neutralino.js'),
-        format    : 'iife',
-        name      : 'Neutralino',
-        sourcemap : devmode,
-        freeze    : false,
-        esModule  : false
-    })
-})
-.then (({ output }) =>
-{
-    for (var entry of output)
-    {
-        var filepath = joinPath (outdir, entry.fileName)
-
-        if (entry.type === 'chunk')
-        {
-            // rollup-plugin-ts does not move the map in individual chunk
-            if (entry.map) {
-                write (filepath + '.map', entry.map.toString ())
-                var code = entry.code + '\n//# sourceMappingURL=neutralino.js.map'
-            } else {
-                var code = entry.code
-            }
-            write (filepath, code)
-        }
-        else if (entry.fileName === 'neutralino.d.ts')
-        {
-            var code = entry.source.toString ()
-            writeDts (joinPath (outdir, 'neutralino.d.ts'),
-                    code.substring (code.indexOf("declare namespace"), code.lastIndexOf ("export")))
-        }
-        else
-        {
-            write (filepath, entry.source)
-        }
-    }
-    resetCommitHash()
-})
-.catch (err =>
-{
-    console.error (
-        '\n' + err +
-        // RollupLogProps, https://github.com/rollup/rollup/blob/master/src/rollup/types.d.ts#L24
-        (typeof err.loc   === 'object' ? '\n' + err.loc.file + ':' + err.loc.line : '') +
-        (typeof err.frame === 'string' ? '\n' + err.frame : '')
-    )
-})
-
-function write (filepath, content)
-{
-    console.log ('write', filepath)
-    writeFile (filepath, content, { encoding: 'utf8' }, (err) =>
-    {
-        if (err)
-            console.error (''+err)
-    })
+if (existsSync(outdir)) {
+    rmSync(outdir, { recursive: true });
+} else {
+    mkdirSync(outdir, { recursive: true });
 }
 
-function patchInitFile (search, replace)
-{
-    let initSource = readFileSync ('./src/api/init.ts', { encoding: 'utf8' })
-    initSource = initSource.replace (search, replace)
-    writeFileSync ('./src/api/init.ts', initSource, { encoding: 'utf8' })
+console.log('Preprocessing files...');
+addCommitHash();
+
+console.log('import src/index.ts');
+
+rollup({
+    input: 'src/index.ts',
+    plugins: [
+        Json(),
+        typescript({
+            tsconfig: './tsconfig.json', // Use this tsconfig file to configure TypeScript compilation
+        }),
+        devmode
+            ? cleanup({ comments: 'none' })
+            : Minify({ format: { comments: false } }),
+    ],
+})
+    .then(async build => {
+        console.log('generate dist/neutralino.mjs');
+
+        await build.write({
+            file: joinPath(outdir, 'neutralino.mjs'),
+            format: 'esm',
+            name: 'Neutralino',
+            sourcemap: devmode,
+        });
+
+        console.log('generate lib');
+
+        await build.write({
+            dir: outdir,
+            format: 'cjs',
+            sourcemap: devmode,
+        });
+
+        console.log('generate dist/neutralino.js');
+
+        await build.write({
+            file: joinPath(outdir, 'neutralino.js'),
+            format: 'iife',
+            name: 'Neutralino',
+            sourcemap: devmode,
+            freeze: false,
+            esModule: false,
+        });
+        return 0;
+    })
+    .then(returned => {
+        console.log('Rolling up typescript declarations');
+
+        const bundled = generateDtsBundle([
+            {
+                filePath: './dist/declarations/index.d.ts',
+                output: {
+                    inlineDeclareGlobals: true,
+                    inlineDeclareExternals: true,
+                    noBanner: true,
+                    exportReferencedTypes: false,
+                },
+            },
+        ]);
+        const bundledUnderNamespace = generateDtsBundle([
+            {
+                filePath: './dist/declarations/index.d.ts',
+                output: {
+                    inlineDeclareGlobals: true,
+                    inlineDeclareExternals: true,
+                    noBanner: true,
+                    exportReferencedTypes: false,
+                    umdModuleName: 'Neutralino',
+                },
+            },
+        ]);
+        write(joinPath(outdir, 'neutralino.d.ts'), bundledUnderNamespace[0]);
+        write(joinPath(outdir, 'neutralino.d.mts'), bundled[0]);
+        write(joinPath(outdir, 'index.d.ts'), bundled[0]);
+        rmSync(joinPath(outdir, 'declarations'), { recursive: true });
+        resetCommitHash();
+    })
+    .catch(err => {
+        console.error(
+            '\n' +
+                err +
+                // RollupLogProps, https://github.com/rollup/rollup/blob/master/src/rollup/types.d.ts#L24
+                (typeof err.loc === 'object'
+                    ? '\n' + err.loc.file + ':' + err.loc.line
+                    : '') +
+                (typeof err.frame === 'string' ? '\n' + err.frame : ''),
+        );
+    });
+
+function write(filepath, content) {
+    console.log('write', filepath);
+    writeFile(filepath, content, { encoding: 'utf8' }, err => {
+        if (err) console.error('' + err);
+    });
+}
+
+function patchInitFile(search, replace) {
+    let initSource = readFileSync('./src/api/init.ts', { encoding: 'utf8' });
+    initSource = initSource.replace(search, replace);
+    writeFileSync('./src/api/init.ts', initSource, { encoding: 'utf8' });
 }
 
 function addCommitHash() {
     try {
-        const hash = exec('git log -n 1 main --pretty=format:"%H"').toString().trim();
+        const hash = exec('git log -n 1 main --pretty=format:"%H"')
+            .toString()
+            .trim();
         patchInitFile('<git_commit_hash_latest>', hash);
         commitHash = hash;
     } catch (err) {
@@ -147,66 +156,6 @@ function addCommitHash() {
         commitHash = '<git_commit_hash_latest>';
     }
 }
-function resetCommitHash ()
-{
-    patchInitFile (commitHash, '<git_commit_hash_latest>')
-}
-
-// Function to fetch all .ts files from the typings directory
-const getTypeDefinitionFiles = () => {
-    const typingsDir = './src/types'; // Path to your typings directory
-    try {
-        const files = readdirSync(typingsDir);
-        return files.filter(file => file.endsWith('.ts'));
-    } catch (error) {
-        console.error('Error reading typings directory:', error);
-        return [];
-    }
-};
-
-// Fetch all .d.ts files from the typings directory
-const typeFiles = getTypeDefinitionFiles();
-
-const writeDts = (filepath, definitions) => {
-    // A 'declare' modifier cannot be used in an already ambient context.
-    definitions = definitions.replaceAll ('declare namespace', 'namespace')
-    definitions = definitions.replaceAll('export enum', 'declare enum')
-    definitions = definitions.replaceAll ('declare function', 'function')
-
-    // Read the type definition files
-    let typesSource = ''
-    typeFiles.forEach(file => {
-        let source = readFileSync (`./src/types/${file}`, { encoding: 'utf8' }) + '\n\n';
-        // Remove export keyword in the ambient declaration file
-        source = source.replaceAll ('export ', '');
-        typesSource += source;
-    });
-
-    let globalsSource = readFileSync ('./src/index.ts', { encoding: 'utf8' })
-    let globals = globalsSource.substring(globalsSource.indexOf('// --- globals ---'),
-                        globalsSource.lastIndexOf('// --- globals ---'))
-                        .trim()
-                        .split('\n')
-                        .map((s) => s.includes('NL_') ? s.replace(/NL_/g, 'declare const $&') : s )
-                        .map((s) => s.trim())
-                        .join('\n')
-
-    write (filepath,
-`// Type definitions for Neutralino ${version}
-// Project: https://github.com/neutralinojs
-// Definitions project: https://github.com/neutralinojs/neutralino.js
-
-declare namespace Neutralino {
-
-${definitions}
-}
-
-${typesSource}
-${globals}
-
-${
-    // rollup-plugin-ts produce an empty map, maybe we will find a solution in the future.
-    // devmode ? '//# sourceMappingURL=neutralino.d.ts.map' : ''
-    ''
-}`/*dtsTemplate*/)
+function resetCommitHash() {
+    patchInitFile(commitHash, '<git_commit_hash_latest>');
 }
