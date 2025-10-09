51a40ccfec920aaf368f8f85504b089d3906262d
Shalitha Suranga <shalithasuranga@gmail.com>
2025-10-09 17:48:41 +0530

    Add storage.clear() and storage.removeData()

--- src/api/storage.ts
+++ src/api/storage.ts
@@ -1,6 +1,6 @@
 import { sendMessage } from '../ws/websocket';
 
-export function setData(key: string, data: string): Promise<void> {
+export function setData(key: string, data: string | null): Promise<void> {
     return sendMessage('storage.setData', { key, data });
 };
 
@@ -8,6 +8,14 @@ export function getData(key: string): Promise<string> {
     return sendMessage('storage.getData', { key });
 };
 
+export function removeData(key: string): Promise<void> {
+    return sendMessage('storage.removeData', { key });
+};
+
 export function getKeys(): Promise<string[]> {
     return sendMessage('storage.getKeys');
+};
+
+export function clear(): Promise<void> {
+    return sendMessage('storage.clear');
 };
