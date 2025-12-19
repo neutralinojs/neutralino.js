8ff41f3da84dcaa952de3547eaaed9b1e5512958
subhu339 <sk3656770@gmail.com>
2025-12-19 14:42:58 +0530

    fix(types): correct return type of server.getMounts

--- src/api/server.ts
+++ src/api/server.ts
@@ -1,5 +1,13 @@
 import { sendMessage } from '../ws/websocket';
 
+export interface Mount {
+    source: string;
+    isReadOnly: boolean;
+}
+
+export type Mounts = Record<string, Mount>;
+
+
 export function mount(path: string, target: string): Promise<void> {
     return sendMessage('server.mount', { path, target });
 }
@@ -8,6 +16,6 @@ export function unmount(path: string): Promise<void> {
     return sendMessage('server.unmount', { path });
 }
 
-export function getMounts(): Promise<void> {
+export function getMounts(): Promise<Mounts> {
     return sendMessage('server.getMounts');
 }
